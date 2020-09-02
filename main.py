0c8aa264c0c476b701f04ffb6aa0a71289bf46d8
melvin <pm18008@ues.edu.sv>
2020-09-02 10:12:06 -0600

    primer commit

---
+++ main.py
@@ -0,0 +1,15 @@
+# Realizar piramide y decirle hasta que nivel se puede
+
+num = input('Ingrese el numero de bloques: ')
+
+try:
+    n = int(num)
+except:
+    print('valor no valido')
+
+ite = 0
+while n > ite:
+    ite = ite + 1
+    n = n - ite
+
+print('Se forma la piramide completa hasta el nivel:', ite)
