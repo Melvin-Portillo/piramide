031192db38bb6debe9b4b06358233e1fe8bca08c
melvin <pm18008@ues.edu.sv>
2020-09-06 08:06:22 -0600

    piramide correjida

--- main.py
+++ main.py
@@ -11,6 +11,10 @@ ite = 0
 while n > ite:
     ite = ite + 1
     n = n - ite
-    print('*'*ite)
+
+nivel = 1
+while nivel <= ite :
+    print(' '*(ite-nivel), '* '*nivel, sep='')
+    nivel=nivel+1
 
 print('Se forma la piramide completa hasta el nivel:', ite)
